07705c87ee67e25e3657c427ee04e76f1aa37b58
Argian <136880933@qq.com>
2021-11-15 18:31:58 -0800

    Micro frontend demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
+import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MicroApp1Component } from './micro-app1/micro-app1.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { createCustomElement } from '@angular/elements';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MicroApp1Component,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +20,13 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap{
+  constructor(private injector: Injector){
+
+  }
+
+  ngDoBootstrap(appRef: ApplicationRef): void {
+    const microApp1 = createCustomElement(MicroApp1Component, {injector: this.injector});
+    customElements.define('app-one', microApp1);    
+  }
+}
---
+++ src/app/micro-app1/micro-app1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-micro-app1',
+  templateUrl: './micro-app1.component.html',
+  styleUrls: ['./micro-app1.component.scss']
+})
+export class MicroApp1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
